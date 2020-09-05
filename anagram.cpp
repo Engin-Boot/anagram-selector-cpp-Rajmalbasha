2a3dbbd593494aa761de5f02637f5e0406de9977
Rajmalbasha <68477960+Rajmalbasha@users.noreply.github.com>
2020-09-05 21:51:42 +0530

    Update anagram.cpp

--- anagram.cpp
+++ anagram.cpp
@@ -1,18 +1,20 @@
 #include "anagram.h"
-#define no_of_chars 256
+
 bool Anagram::WordPairIsAnagram(const std::string& word1, const std::string& word2) {
-    int count[no_of_chars]={0}
-     for (i = 0; str1[i] && str2[i]; i++) { 
-        count[str1[i]]++; 
-        count[str2[i]]--; 
-    } 
-    if (str1[i] || str2[i]) 
-        return false; 
-    
-    for (i = 0; i < NO_OF_CHARS; i++) 
-        if (count[i]) 
-            return false; 
-    return true; 
+  int count[26]={0};
+    for (int i=0;word1[i]&&word2[i];i++)
+    {   
+        if (word1(i)!=' ')
+            count[tolower(word1(i))-97]++;
+        if(word2(i)!=' ') 
+            count[tolower(word2(i))-97]--;
+    }
+    for (int i=0;i<26;i++)
+    {
+        if(count[i])
+            return false;
+    }
+    return true;
   
 }
 
@@ -22,10 +24,11 @@ std::vector<std::string> Anagram::SelectAnagrams(
     vector <string> Candidates={}
     for (int i=0;i<candidates.size();i++)
     {
-        if (WordPairIsAnagram(word,candidates[i])
+        
+         if(WordPairIsAnagram(word,candidates[i])
             {
                 Candidates.push_back(candidates[i]);
             }
     }
-    return candidates;
+    return Candidates;
 }
